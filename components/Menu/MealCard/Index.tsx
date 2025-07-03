1d2066ca4fae50e66ebbcb12ba943be6d572d471
Cirrous <christianraisch@gmail.com>
2025-07-03 11:42:11 +0200

    Added price and Category to the new meal cards

--- components/Menu/MealCard/Index.tsx
+++ components/Menu/MealCard/Index.tsx
@@ -10,13 +10,26 @@ export const MealCard = ({
   item: Meal;
   index: number;
 }) => {
+  /*
+  "Student = 0, Angestellte = 1, Gäste = 2.
+  TODO: Später durch die in Einstellungen gespeicherte Einstellung für Preise ersetzen. 
+  */
+  const preis_einstellung = [0,1,2]
+
   return (
     <TouchableOpacity className="flex-1 mx-2 last:flex">
       <Card variant="elevated" className="outline outline-1 p-4 mb-4 flex-1">
         {/* Meal name */}
-        <Text className="font-roboto text-sm">{meal.name.trim()}</Text>
+        <Text className="font-roboto text-sm"
+        numberOfLines={2}
+        ellipsizeMode="tail">
+        {meal.name.trim()}</Text>
         {/* Price */}
-        {/* <Text>{meal.prices ? `${meal.prices} €` : 'Preis nicht verfügbar'}</Text> */}
+        {<Text> {meal.prices && meal.prices.length > 0
+            ? `${(meal.prices[preis_einstellung[0]]).price.toFixed(2)}€`
+            : 'Kein Preis'}</Text> }
+        {/* Category */}
+        <Text>{meal.category}</Text>
       </Card>
     </TouchableOpacity>
   );
