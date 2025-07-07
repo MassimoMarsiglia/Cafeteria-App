1f2b61a70be13d7dc28a1644328001847d4f1f23
Cirrous <christianraisch@gmail.com>
2025-07-07 15:08:40 +0200

    updated image selection logic

--- components/Menu/MealCard/Index.tsx
+++ components/Menu/MealCard/Index.tsx
@@ -13,24 +13,49 @@ export const MealCard = ({
   index: number;
   priceCategory: number;
 }) => {
+  //Bildauswahl für Card // Salate Suppen Aktionen Essen Beilagen Desserts
+  const getCategoryImage = (category: string | undefined) => {
+    switch ((category ?? '').toLowerCase()) {
+      case 'essen':
+        return require('@/assets/categorys/essen.png');
+      case 'beilagen':
+        return require('@/assets/categorys/beilagen.png');
+      case 'salate':
+        return require('@/assets/categorys/salate.png');
+      case 'suppen':
+        return require('@/assets/categorys/suppen.png');
+      case 'desserts':
+        return require('@/assets/categorys/desserts.png');
+      default:
+        return require('@/assets/categorys/default.png'); // Fallback-Bild
+    }
+  };
 
   return (
     <TouchableOpacity className="flex-1 mx-2 last:flex">
       <Card variant="elevated" className="outline outline-1 p-2 mb-4 flex-1">
-      <Image
-        source={require("@/assets/categorys/essen.png")}
-        style={{ height: 100, width: '100%', marginBottom: 8, borderRadius: 8 }}
-        alt="image"
-      />
+        <Image
+          source={getCategoryImage(meal.category)}
+          style={{
+            height: 100,
+            width: '100%',
+            marginBottom: 8,
+            borderRadius: 8,
+          }} // Tailwind hat nicht funktioniert
+          alt="image"
+        />
         {/* Meal name */}
-        <Text className="font-roboto"
-        numberOfLines={2}
-        ellipsizeMode="tail">
-        {meal.name.trim()}</Text>
+        <Text className="font-roboto" numberOfLines={2} ellipsizeMode="tail">
+          {meal.name.trim()}
+        </Text>
         {/* Price */}
-        {<Text>{meal.prices && meal.prices.length > 0
-            ? `${(meal.prices[Number(priceCategory)]).price.toFixed(2)}€`
-            : 'Kein Preis'}</Text> }
+        {
+          <Text>
+            {meal.prices && meal.prices.length > 0
+              ? `${meal.prices[Number(priceCategory)].price.toFixed(2)}€`
+              : 'Kein Preis'}
+          </Text>
+        }
         {/* Category */}
         <Text>{meal.category}</Text>
       </Card>
