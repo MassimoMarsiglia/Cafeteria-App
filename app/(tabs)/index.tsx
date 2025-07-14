7f38329c040dd0b606548e3255c527a54aa15ed5
Hang Truong <thanhhang2x2@gmail.com>
2025-07-14 22:02:53 +0200

    Hompage demo
    Only show for the first time access

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,61 @@
-import { View } from 'react-native';
+import { useSettings } from '@/hooks/redux/useSettings';
+import { router } from 'expo-router';
+import React, { useEffect } from 'react';
+import {
+  Alert,
+  ImageBackground,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 const HomeView = () => {
-  return <View></View>;
+  const { favoriteCanteen } = useSettings();
+
+  useEffect(() => {
+    if (favoriteCanteen?.id) {
+      router.replace(`/menu/${favoriteCanteen.id}`);
+    }
+  }, [favoriteCanteen]);
+
+  const handleNavigate = () => {
+    Alert.alert(
+      'Anweisung',
+      'Wähle deine Lieblingsmensa, indem du auf das ❤️-Icon klickst.',
+      [
+        { text: 'Abbrechen', style: 'cancel' },
+        {
+          text: 'Weiter',
+          onPress: () => router.navigate('/mensen/mensenList'),
+        },
+      ],
+    );
+  };
+
+  return (
+    <ImageBackground
+      source={require('@/assets/welcome-animation-2.gif')}
+      resizeMode="cover"
+      className="flex-1"
+    >
+      <View className="flex-1 bg-black/70 justify-center items-center px-6">
+        <Text className="text-white text-3xl font-bold mt-6 text-center">
+          Willkommen bei Schmausa
+        </Text>
+        <Text className="text-gray-400 text-base mt-2 text-center">
+          Erstellt von Schmausa-Team
+        </Text>
+        <TouchableOpacity
+          onPress={handleNavigate}
+          className="mt-10 bg-blue-500 px-6 py-3 rounded-lg"
+        >
+          <Text className="text-white text-base font-semibold">
+            Lieblingsmensa wählen
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  );
 };
 
 export default HomeView;
--- components/Navbar/Index.tsx
+++ components/Navbar/Index.tsx
@@ -1,3 +1,4 @@
+import { usePathname } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Sidebar } from '../Sidebar/Index';
 import { Box } from '../ui/box';
@@ -7,6 +8,13 @@ import { Settingsbutton } from './Settingsbutton';
 import { SidebarButton } from './SidebarButton/Index';
 
 export const Navbar = () => {
+  const pathname = usePathname();
+
+  // Hide Navbar on homepage
+  if (pathname === '/') {
+    return null;
+  }
+
   return (
     <SafeAreaView edges={['top']} className="bg-background-0">
       <Box className="flex-row items-center justify-between px-4 py-1 bg-background-0">
