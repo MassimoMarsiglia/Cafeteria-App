62674f1a58d5133aa4f1abcc434433f6663a37d4
Massimo Marsiglia <marsigliamassi@gmail.com>
2025-07-15 02:05:33 +0200

    fixed react hook dependency array

--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -172,7 +172,7 @@ export default function SettingsScreen() {
       favoriteCanteen,
       favoriteMealsIcon,
       savedRecipesIcon,
-      notificationsIcon,
+      handleFavoriteMealsPress,
     ],
   );
 
